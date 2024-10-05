ceb0b816bf0bb943d781ca1d0ca2c35ebb5606b4
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-10-05 18:22:32 +0530

    backend init

---
+++ native-backend/src/main/java/com/native_backend/native_backend/NativeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.native_backend.native_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeBackendApplication.class, args);
+	}
+
+}
---
+++ native-backend/src/test/java/com/native_backend/native_backend/NativeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.native_backend.native_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
